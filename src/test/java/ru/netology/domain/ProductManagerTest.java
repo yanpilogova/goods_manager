1798e47b80f41b36be1c8466d880120bd7ff1ff0
yanpilogova <yulia.anpilogova@noveogroup.com>
2022-05-02 11:19:59 +0700

    added files

---
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -0,0 +1,66 @@
+package ru.netology.domain;
+
+import org.junit.jupiter.api.Test;
+import ru.netology.repository.ProductRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductManagerTest {
+
+    @Test
+    void getRepository() {
+
+    }
+
+    @Test
+    void setRepository() {
+    }
+
+    @Test
+    void addProducts() {
+        Product bookOne = new Book(1, "Cake book", 1010, "Ivanov A.V.");
+        Product smartphoneOne = new Smartphone(3,"Nokia",11200, "Nokia Lmt");
+
+        Product[] expected = new Product[]{bookOne, smartphoneOne};
+        ProductManager manager = new ProductManager();
+        manager.add(bookOne);
+        manager.add(smartphoneOne);
+
+        assertArrayEquals(expected, manager.findAll());
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void removedByID() {
+        Product bookOne = new Book(1, "Cake book", 1010, "Ivanov A.V.");
+
+        Product[] expected = new Product[0];
+        ProductManager manager = new ProductManager();
+        manager.add(bookOne);
+        manager.removedByID(1);
+
+        assertArrayEquals(expected, manager.findAll());
+    }
+
+    @Test
+    void searchByName() {
+        Product bookOne = new Book(1, "Cake book", 1010, "Ivanov A.V.");
+        Product smartphoneOne = new Smartphone(3,"Nokia",11200, "Nokia Lmt");
+
+        Product[] expected = new Product[]{bookOne};
+        ProductManager manager = new ProductManager();
+        manager.add(bookOne);
+        manager.add(smartphoneOne);
+
+        Product[] actual = manager.searchBy("Cake");
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void matches() {
+    }
+}
