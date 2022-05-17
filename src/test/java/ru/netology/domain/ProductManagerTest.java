a94e5c304d31e4a90ec0d25167e0a3fe01568efd
yanpilogova <yulia.anpilogova@noveogroup.com>
2022-05-17 16:33:03 +0700

    added tests

--- src/test/java/ru/netology/domain/ProductManagerTest.java
+++ src/test/java/ru/netology/domain/ProductManagerTest.java
@@ -44,18 +44,44 @@ class ProductManagerTest {
 
         assertArrayEquals(expected, manager.findAll());
     }
+    @Test
+    void searchByNameNoData() {
+        ProductRepository repository = new ProductRepository();
+        ProductManager manager = new ProductManager();
+        Product[] expected = {};
+        Product[] actual = manager.searchBy("Cake");
+
+        assertArrayEquals(expected, actual);
+    }
 
     @Test
-    void searchByName() {
+    void searchByNameOne() {
         Product bookOne = new Book(1, "Cake book", 1010, "Ivanov A.V.");
-        Product smartphoneOne = new Smartphone(3, "Nokia", 11200, "Nokia Lmt");
 
         Product[] expected = new Product[]{bookOne};
         ProductManager manager = new ProductManager();
         manager.add(bookOne);
-        manager.add(smartphoneOne);
 
         Product[] actual = manager.searchBy("Cake");
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    void searchByNameTwo() {
+        Product bookOne = new Book(1, "Cake book", 1010, "Ivanov A.V.");
+        Product bookTwo = new Book(2, "Cool book", 110, "Petrov A.V.");
+        Product smartphoneOne = new Smartphone(3, "Nokia", 11200, "Nokia Lmt");
+        Product smartphoneTwo = new Smartphone(4, "Nokia", 12200, "Nokia LTD");
+
+
+        Product[] expected = new Product[]{smartphoneOne, smartphoneTwo};
+        ProductManager manager = new ProductManager();
+        manager.add(bookOne);
+        manager.add(bookTwo);
+        manager.add(smartphoneOne);
+        manager.add(smartphoneTwo);
+
+        Product[] actual = manager.searchBy("Nokia");
 
         assertArrayEquals(expected, actual);
     }
